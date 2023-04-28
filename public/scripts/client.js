f99bdf9ac7733ef4df251903b3d29af9ee4b8758
DanRoss88 <danielmartinross@gmail.com>
2023-04-27 17:00:33 -0700

    cleaned it up

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,68 +4,62 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  
-  
-    const $errorOver = $('#error-msg-over').hide();
-    const $errorNull = $('#error-msg-null').hide();
-    const $form = $('#tweet-form').hide();
 
-    $("#new-tweet-button").click(() => {
-      $form.slideToggle().find('textArea').focus();
-    });
+  //// VARIABLES ////
+  const $errorOver = $('#error-msg-over').hide();
+  const $errorNull = $('#error-msg-null').hide();
+  const $form = $('#tweet-form').hide();
 
+  //////// NAVBAR BUTTON //////
+  $("#new-tweet-button").click(() => {
+    $form.slideToggle().find('textArea').focus();
+  });
+  /////////
   const createTweetElement = function(tweetData) {
-    
+    //TIMEAGO//
     const dateCreated = timeago.format(tweetData.created_at);
-    
-    
+    //ESCAPE//
     const escape = function(str) {
       let div = document.createElement("div");
       div.appendChild(document.createTextNode(str));
       return div.innerHTML;
     };
-
+    ///// TWEET ARTICLE /////
     const tweetForm = `
-    <article class="tweet">
-      <header class="tweet-header">
-        <img src="${tweetData.user.avatars}" alt="User avatar">
-        <p>${tweetData.user.name}</p>
-        <p>${tweetData.user.handle}</p>
-      </header>
-      <div class="tweet-content">
-        ${escape(tweetData.content.text)}
-      </div>
-      <footer>
-        <span class="tweet-date">${dateCreated}</span>
-        <div class="actions">
-          <i class="fa fa-flag"></i>
-          <i class="fa fa-retweet"></i>
-          <i class="fa fa-heart"></i>
-        </div>
-      </footer>
-    </article>
-  `;
-
-    
+      <article class="tweet">
+        <header class="tweet-header">
+          <img src="${tweetData.user.avatars}" alt="User avatar">
+            <p>${tweetData.user.name}</p>
+            <p>${tweetData.user.handle}</p>
+            </header>
+              <div class="tweet-content">
+               ${escape(tweetData.content.text)}
+              </div>
+              <footer>
+              <span class="tweet-date">${dateCreated}</span>
+              <div class="actions">
+              <i class="fa fa-flag"></i>
+              <i class="fa fa-retweet"></i>
+              <i class="fa fa-heart"></i>
+            </div>
+          </footer>
+        </article>
+      `;
     return tweetForm;
   };
-
+    
+////************** RENDER TWEETS **************////
   const renderTweets = function(tweets) {
     
-    const $tweetsContainer = $('#tweets-container').empty(); 
-    
-    
+    const $tweetsContainer = $('#tweets-container').empty();
+      
     for (const tweet of tweets) {
-     
-      const $tweet = createTweetElement(tweet);
-
+    const $tweet = createTweetElement(tweet);
       $tweetsContainer.prepend($tweet);
-
     }
   };
-
+////************* LOAD TWEETS **************////
   const loadTweets = function() {
-  
     
     $.ajax({
       url: '/tweets',
@@ -73,7 +67,7 @@ $(document).ready(function() {
       dataType: 'json'
     })
       .then(function(response) {
-       
+
         renderTweets(response);
       })
       .catch(function(error) {
@@ -86,41 +80,37 @@ $(document).ready(function() {
   $errorNull.slideUp();
   $errorOver.slideUp();
 
-  
-   $form.submit(function(event) {
-    event.preventDefault();
+///////////*************  POST FORM *************////////////////// 
+  $form.submit(function(event) {
     
+    event.preventDefault();
     const tweetText = $('#tweet-text').val();
-       
-      
+
+    ///Error validations///
     if (!tweetText) {
       $errorNull.slideDown();
       return;
-    } 
+    }
     if (tweetText.length > 140) {
       $errorOver.slideDown();
       return;
     }
-    
-   
+
     const formData = $(this).serialize();
-    
-    
+
     $.ajax({
       url: '/tweets',
       method: 'POST',
       data: formData
     })
-
       .then(loadTweets);
-
-      $errorNull.slideUp(); 
+    
+    //Error notifications, text value, and counter all reset//
+      $errorNull.slideUp();
       $errorOver.slideUp();
     $('#tweet-text').val('');
-      
-    
     $('.new-tweet .counter').text('140');
-      
+
   });
 
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,24 +1,16 @@
 
 $(document).ready(function() {
-  
-  let textarea = $('.new-tweet form textarea');
-  let counter = textarea.siblings('.counter');
 
+  let textarea = $('.new-tweet form textarea');
 
   textarea.on('input', function() {
 
-    let length = $(this).val().length;
-    console.log('Input length line 14', length);
+    let textLength = $(this).val().length;
+    let charLeft = 140 - textLength;
 
-    
-    let remaining = 140 - length;
-    console.log('remaining characters line, 18', remaining);
+    $('.new-tweet .counter').text(charLeft);
 
-
-    $('.new-tweet .counter').text(remaining);
-
-    
-    if (remaining < 0) {
+    if (charLeft < 0) {
       $('.counter').css('color', 'red');
     } else {
       $('.counter').css('color', 'black');
@@ -26,4 +18,4 @@ $(document).ready(function() {
 
   });
 });
-  
+
